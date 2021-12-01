a2c0c23c5ee359b2d09b8977b8d484ced1bd1e97
SaiCharan1103 <92868531+SaiCharan1103@users.noreply.github.com>
2021-12-01 13:14:23 +0530

    Before Day 2
    Starting other projects

--- PaymentsService/src/main/java/CaseStudy/PaymentsService/HelloController3.java
+++ PaymentsService/src/main/java/CaseStudy/PaymentsService/HelloController3.java
@@ -1,11 +1,13 @@
 package CaseStudy.PaymentsService;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController	
+@RequestMapping("/hello")
 public class HelloController3 {
-		@RequestMapping("/hello")
+		@GetMapping("/hello")
 		public String helloworld() {
 			return "Hello World!";
 		}
